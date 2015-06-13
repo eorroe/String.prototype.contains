33d862c33450dc48cb368d294343adb123a3e664
Edwin Reynoso <eorroe@gmail.com>
2015-06-13 14:20:26 -0400

    Update contains.js

--- contains.js
+++ contains.js
@@ -1,6 +1,6 @@
 String.prototype.contains = function(strings, index) {
 	if(!Array.isArray(strings)) throw Error('1 argument is not an array');
-	return strings.some(string => return this.includes(string, index), this);
+	return strings.some(string => this.includes(string, index), this);
 }
 
 // This should most likely be longer, but I need help improving it. Not sure how to
